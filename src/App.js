78d96ecb3c59054eb1a210b0470128803b700fb6
vmaxtacular513 <veronicamaxwell152@gmail.com>
2020-10-24 16:07:35 -0400

    Add styles to title and background

--- src/App.js
+++ src/App.js
@@ -13,12 +13,18 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
+    
   }
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users =>  this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
   }
 
     render() {
@@ -29,11 +35,10 @@ class App extends Component {
         )
       return (
       <div className='App'>
-        
+        <h1> Rolodex </h1>
         <SearchBox 
           placeholder='search monster'
-          handleChange={e => 
-          this.setState({ searchField: e.target.value })}
+          handleChange={ this.handleChange }
         />
         <CardList monsters={filteredMonsters} />
       </div>
